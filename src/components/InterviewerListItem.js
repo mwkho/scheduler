ec19dc6d402e1019c87ab4bd90dd8046e3aa16af
Martin Ho <martin.h@hotmail.ca>
2021-04-13 14:20:44 -0700

    Fix bug  in InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -8,7 +8,7 @@ const InterviewerListItem = (props) => {
   const interviewerClass = classNames('interviewers__item', {"interviewers__item--selected":props.selected});
 
   return (
-    <li key={props.id} onClick={() => {props.setInterviewer(props.name)}} className={interviewerClass}>
+    <li key={props.id} onClick={() => {props.setInterviewer(props.id)}} className={interviewerClass}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
