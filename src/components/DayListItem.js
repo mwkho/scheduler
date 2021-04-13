fb312e5d1cb0bc307fe90c502ca1ec64eeb4a5ca
Martin Ho <martin.h@hotmail.ca>
2021-04-13 09:48:07 -0700

    Make DayListItem test pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,25 @@
 import React, { useState} from 'react'
+import './DayListItem.scss'
+
 const classNames = require('classnames');
 
 
 export default  function DayListItem(props) {
+  const dayListClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 
+  'day-list__item--full': !props.spots})
+  const spotsRemaining = props.spots;
+
+  function formatSpotsRemaining (spots) {
+    if (spotsRemaining){
+      return `${spotsRemaining} ${spotsRemaining > 1 ? 'spots' : 'spot'} remaining`;
+    }
+    return 'no spots remaining';
+
+  }
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className='text--regular'>{props.name}</h2>
+    <li className={dayListClass} onClick={() => props.setDay(props.name)}>
+      <h2 className='text--regular'>{formatSpotsRemaining(spotsRemaining)}</h2>
       <h3 className='text--light'> {props.spots} spots</h3>
     </li>
   )
