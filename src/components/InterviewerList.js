fc749182e1e124538e856baa052dee1f283dd561
Martin Ho <martin.h@hotmail.ca>
2021-04-13 14:14:16 -0700

    Build interviewerList using props and stylized

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
 
-const InterviewerList = (props) => {
+import './InterviewerList.scss'
 
+const InterviewerList = (props) => {
+  const interviewers = props.interviewers.map((interviewer) => {
+
+    return (<InterviewerListItem 
+      id={interviewer.id} 
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+      />)
+  });
   return (
     <section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"></ul>
-</section>
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
   );
 };
 
