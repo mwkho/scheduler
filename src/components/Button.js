599544aedfee0273b80b25e86ceccfe99a7c8349
Martin Ho <martin.h@hotmail.ca>
2021-04-13 01:10:22 -0700

    Make button style with className

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from "react";
-
 import "components/Button.scss";
 
+const classNames = require('classnames');
+
 export default function Button(props) {
-   return <></>;
+   const {onClick} = props
+   let buttonClass = classNames('button', {"button--danger":props.danger, 
+            "button--confirm":props.confirm});  
+            
+   return <button disabled={props.disabled} onClick={onClick} className={buttonClass}>{props.children}</button>;
 }
