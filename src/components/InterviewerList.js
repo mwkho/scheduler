909595306a0a20870e2592feae24b5ba18dcc885
Martin Ho <martin.h@hotmail.ca>
2021-04-13 13:44:00 -0700

    Initial InterviewerList build w/ stories

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import InterviewerListItem from './InterviewerListItem';
+
+const InterviewerList = (props) => {
+
+  return (
+    <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list"></ul>
+</section>
+  );
+};
+
+export default InterviewerList;
--- stories/index.js
+++ stories/index.js
@@ -36,35 +36,35 @@ import InterviewerListItem from 'components/InterviewerListItem';
 // .add("Clickable", () => (
 //   <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
 // ));
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
-storiesOf("DayList", module)
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
-  })
-  .add("Monday", () => (
-    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
-  ))
-  .add("Tuesday", () => (
-    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
-  ));
-  
+// storiesOf("DayList", module)
+//   .addParameters({
+//     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+//   })
+//   .add("Monday", () => (
+//     <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+//   ))
+//   .add("Tuesday", () => (
+//     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+//   ));
+
   const interviewer = {
     id: 1,
     name: "Sylvia Palmer",
@@ -98,3 +98,34 @@ storiesOf("InterviewerListItem", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+
+
+  const interviewers = [
+    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+  ];
+  
+  storiesOf("InterviewerList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    })
+    .add("Initial", () => (
+      <InterviewerList
+        interviewers={interviewers}
+        setInterviewer={action("setInterviewer")}
+      />
+    ))
+    .add("Preselected", () => (
+      <InterviewerList
+        interviewers={interviewers}
+        interviewer={3}
+        setInterviewer={action("setInterviewer")}
+      />
+    ));
+  
+
+
