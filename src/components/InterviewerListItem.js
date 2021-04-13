802e791fcfa09a2680438226d475cbb632851fdc
Martin Ho <martin.h@hotmail.ca>
2021-04-13 13:37:42 -0700

    Build InterviewerListItem with stories

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import 'InterviewerListItem.scss'
+import './InterviewerListItem.scss'
 
 const classNames = require('classnames');
 
 
-export default InterviewerListItem = (props) => {
-  const interviewerClass = classNames('interviewers__item', {"interviewers__item":props.selected});
+const InterviewerListItem = (props) => {
+  const interviewerClass = classNames('interviewers__item', {"interviewers__item--selected":props.selected});
 
   return (
-    <li key={props.id} className={interviewerClass}>
+    <li key={props.id} onClick={() => {props.setInterviewer(props.name)}} className={interviewerClass}>
   <img
     className="interviewers__item-image"
     src={props.avatar}
     alt="Sylvia Palmer"
   />
-  {props.name}
+  {props.selected ? props.name:''}
 </li>
   )
 }
+
+export default InterviewerListItem;
