af18eddf1e2975117857294d4614bcf8503949a6
Martin Ho <martin.h@hotmail.ca>
2021-04-13 02:03:37 -0700

    Refactor DaylistItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState} from 'react'
+const classNames = require('classnames');
+
 
 export default  function DayListItem(props) {
   return (
-    <li>
-      <h2 className='text--regular'>Day Name</h2>
-      <h3 className='text--light'> X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className='text--regular'>{props.name}</h2>
+      <h3 className='text--light'> {props.spots} spots</h3>
     </li>
   )
 }
