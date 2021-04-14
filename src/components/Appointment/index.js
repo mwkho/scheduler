a4dff824c8167b9427064a4a6b0eaec077a9bf89
Martin Ho <martin.h@hotmail.ca>
2021-04-13 17:59:11 -0700

    Make the Initial appointment componenent and header

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss'
+
+const Appointment = () => {
+  return (
+    <article className="appointment"></article>
+  );
+};
+
+
+export default Appointment;
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,8 @@ import DayListItem from "components/DayListItem";
 import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
+import Appointment from 'components/Appointment/';
+import Header from 'components/Appointment/header';
 
 // storiesOf("Button", module)
 //   .addParameters({
@@ -127,6 +129,13 @@ storiesOf("InterviewerListItem", module)
         setInterviewer={action("setInterviewer")}
       />
     ));
+
+
+    storiesOf("Appointment", module)
+    .addParameters({
+      backgrounds: [{ name: "white", value: "#fff", default: true }]
+    })
+    .add("Appointment", () => <Appointment />)
+    .add("Appointment with Time", () => <Appointment time="12pm" />)
+    .add('Header', () => <Header time="12pm"/>)
   
-
-
