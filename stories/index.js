8b0f4695dacfe8299a3996af942c46f8516028e1
Martin Ho <martin.h@hotmail.ca>
2021-04-14 01:04:11 -0700

    Init form comp. and stores w/o state

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
 import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
+import Form from 'components/Appointment/Form';
 
 // storiesOf("Button", module)
 //   .addParameters({
@@ -148,7 +149,8 @@ storiesOf("InterviewerListItem", module)
     .add('Show Hover', () => <Show name={'Lydia Miller-Jones'} interviewer={interviewer} onEdit={action('onEdit')} onDelete={action('onDelete')}/>)
     .add('Confirm', () => <Confirm message='Delete this appointment' onConfirm={action('onConfirm')} onCancel={action('onCancel')}/>)
     .add('Status', () => <Status message='Deleting'/>)
-    .add('Error', () => <Error message='Could not delete appointment' onClose={action('onClose')}/>)
-  
+    .add('Error', () => <Error message={'Could not delete appointment'} onClose={action('onClose')}/>)
+    .add('Form Edit', () => <Form name={'Lydia Miller-Jones'} interviewers={interviewers} interviewer={1} onSave={action('onSave')} onCancel={action('onCancel')} />)
+    .add('Form Create', () => <Form interviewers={interviewers} onSave={action('onSave')} onCancel={action('onCancel')}/>)
 
   
