8ea22ee399231a454483b0f3cf95e1cf0ac53346
Martin Ho <martin.h@hotmail.ca>
2021-04-13 18:09:42 -0700

    Make Empty componenet its stories

--- stories/index.js
+++ stories/index.js
@@ -11,7 +11,8 @@ import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/';
-import Header from 'components/Appointment/header';
+import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
 
 // storiesOf("Button", module)
 //   .addParameters({
@@ -138,4 +139,5 @@ storiesOf("InterviewerListItem", module)
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
     .add('Header', () => <Header time="12pm"/>)
+    .add('Empty', () => <Empty onAdd={action('onAdd')}/>)
   
