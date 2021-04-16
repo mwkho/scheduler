bdac34f7261300f8fa70e1aae4170b46b55be957
Martin Ho <martin.h@hotmail.ca>
2021-04-15 23:33:35 -0700

    Make api calls for appt and days to show

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from './Appointment/'
+import getAppointmentsForDay from "helpers/selectors";
 
 const axios = require('axios');
 
@@ -12,22 +13,24 @@ export default function Application(props) {
     appointments: {}
   });
   
-  const dailyAppointments = [];
-
-  dailyAppointments.map((appointment) => {});
-
   const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
-    axios.get(`/api/days`)
-    .then((response) => {
-      setState((prev) => ({...prev, days: [...response.data]})
-      )
+    Promise.all([
+      axios.get(' http://localhost:8001/api/days'),
+      axios.get(' http://localhost:8001/api/appointments')
+    ])
+    .then((responses) => {
+      setState(prev => ({...prev, days: responses[0].data, appointments: responses[1].data}))
     })
     .catch((err) => {
       console.log(err.response)
     })
   }, []);
+
+  
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
 
   return (
     <main className="layout">
@@ -52,7 +55,7 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {appointments.map((appointment) => {
+        { dailyAppointments.map((appointment) => {
           return(
             <Appointment key={appointment.id} {...appointment} />
           )
