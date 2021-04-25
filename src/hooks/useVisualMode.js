10db7177017bff6e98b292409a4082ce0ad39f9c
Martin Ho <martin.h@hotmail.ca>
2021-04-24 23:03:52 -0700

    Add comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,6 +6,8 @@ const useVisualMode = (initial) => {
 
   const transition = (second, replace = false) => {
     setMode(second);
+
+    // remove the previous state to replace it
     if (replace) { 
       setHistory(prev => prev.slice(0, prev.length - 1));
     }
@@ -13,9 +15,12 @@ const useVisualMode = (initial) => {
   }
 
   const back = () => {
+    // check if there is only one mode in history and set it to be the same
     if (history.length === 1){
       setMode(history[0]);
     }
+    // otherwise reduce the history and get back the last mode in history
+    // the history will always be more than one in this case due to above
     if (history.length > 1) { 
       setMode(history[history.length - 2])
       setHistory(history.slice(0, history.length - 1));
--- stories/index.js
+++ stories/index.js
@@ -19,60 +19,60 @@ import Status from 'components/Appointment/Status';
 import Error from 'components/Appointment/Error';
 import Form from 'components/Appointment/Form';
 
-// storiesOf("Button", module)
-//   .addParameters({
-//     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-//   })
-//   .add("Base", () => <Button>Base</Button>)
-//   .add("Confirm", () => <Button confirm>Confirm</Button>)
-//   .add("Danger", () => <Button danger>Cancel</Button>)
-//   .add("Clickable", () => (
-//     <Button onClick={action("button-clicked")}>Clickable</Button>
-//   ))
-//   .add("Disabled", () => (
-//     <Button disabled onClick={action("button-clicked")}>
-//       Disabled
-//     </Button>
-//   ));
+storiesOf("Button", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Base", () => <Button>Base</Button>)
+  .add("Confirm", () => <Button confirm>Confirm</Button>)
+  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Clickable", () => (
+    <Button onClick={action("button-clicked")}>Clickable</Button>
+  ))
+  .add("Disabled", () => (
+    <Button disabled onClick={action("button-clicked")}>
+      Disabled
+    </Button>
+  ));
 
-// storiesOf("DayListItem", module)
-// .addParameters({
-//   backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-// })
-// .add("Unselected", () => <DayListItem name="Monday" spots={5} />) 
-// .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
-// .add("Full", () => <DayListItem name="Monday" spots={0} />)
-// .add("Clickable", () => (
-//   <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
-// ));
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
+storiesOf("DayListItem", module)
+.addParameters({
+  backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+})
+.add("Unselected", () => <DayListItem name="Monday" spots={5} />) 
+.add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+.add("Full", () => <DayListItem name="Monday" spots={0} />)
+.add("Clickable", () => (
+  <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
+));
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
-// storiesOf("DayList", module)
-//   .addParameters({
-//     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
-//   })
-//   .add("Monday", () => (
-//     <DayList days={days} day={"Monday"} setDay={action("setDay")} />
-//   ))
-//   .add("Tuesday", () => (
-//     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
-//   ));
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));
 
   const interviewer = {
     id: 1,
